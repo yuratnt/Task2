f0f5134ff922f095de25eaa32564a751d576a7b5
tnt <starkov-07@list.ru>
2025-11-26 20:14:34 +0500

    stt update

--- src/main/java/org/tnt/task2/AudioManager/AudioManager.java
+++ src/main/java/org/tnt/task2/AudioManager/AudioManager.java
@@ -1,65 +1,63 @@
 package org.tnt.task2.AudioManager;
 
+import org.json.JSONObject;
+import org.vosk.LibVosk;
+import org.vosk.LogLevel;
+import org.vosk.Model;
+import org.vosk.Recognizer;
+
 import javax.sound.sampled.*;
 import java.io.IOException;
 
+
 public class AudioManager {
 
-    private AudioFormat format = new AudioFormat(
-            44100,
-            16,
-            2,
-            true,
-            true
-    );
-    private DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
 
-    private TargetDataLine targetLine ;
-    private AudioInputStream audioInputStream;
-    Boolean audioStarted = false;
-    public AudioManager() {
-        try {
-            targetLine  = (TargetDataLine) AudioSystem.getLine(info);
-            if (!AudioSystem.isLineSupported(info)) {
-                System.err.println("Микрофон с таким форматом не поддерживается");
-                return;
-            }
-            targetLine  = (TargetDataLine) AudioSystem.getLine(info);
-            audioInputStream = new AudioInputStream(targetLine);
-        } catch (LineUnavailableException e) {
-            throw new RuntimeException(e);
-        }
-
-
-    }
+    private boolean status = false;
+    private String text;
+    private StringBuilder stringBuilder = new StringBuilder();
     public void start() {
         new Thread(() -> {
-            try {
-                targetLine.open(format);
+            LibVosk.setLogLevel(LogLevel.DEBUG);
+            AudioFormat format = new AudioFormat(16000.0f, 16, 1, true, false);
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+            try (Model model = new Model("model/vosk-model-small-ru-0.22");
+                 Recognizer recognizer = new Recognizer(model, 16000)) {
+
+                TargetDataLine microphone = (TargetDataLine) AudioSystem.getLine(info);
+                microphone.open(format);
+                microphone.start();
+
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+
+                status = true;
+                while (status) {
+                    bytesRead = microphone.read(buffer, 0, buffer.length);
+                    if (recognizer.acceptWaveForm(buffer, bytesRead)) {
+                        text = recognizer.getResult();
+                        JSONObject json = new JSONObject(text);
+                        text = json.getString("text");
+                        if (!text.isEmpty()) {
+                            stringBuilder.append(" ");
+                            text = null;
+                        }
+                        stringBuilder.append(json.getString("text"));
+                    }
+                }
             } catch (LineUnavailableException e) {
                 throw new RuntimeException(e);
-            }
-            targetLine.start();
-
-            int bufferSize = (int) ((double) (44100 * 16 * 2) / 8 * 0.1);
-            byte[] buffer = new byte[bufferSize];
-
-            System.out.println("Захват аудио произведён");
-            audioStarted = true;
-            while (audioStarted) {
-                int bytesRead = targetLine.read(buffer, 0, buffer.length);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }).start();
     }
 
     public void stop() {
-        audioStarted = false;
-        targetLine.stop();
-        try {
-            audioInputStream.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        targetLine.close();
+        status = false;
+    }
+
+    public String getText() {
+        return stringBuilder.toString();
     }
 }
--- src/main/java/org/tnt/task2/MainController.java
+++ src/main/java/org/tnt/task2/MainController.java
@@ -1,16 +1,20 @@
 package org.tnt.task2;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.util.Duration;
 import org.tnt.task2.AudioManager.AudioManager;
 
 public class MainController {
-
-    private AudioManager audioManager = new AudioManager();
-
-
+    private static AudioManager audioManager = new AudioManager();
+    private String text;
+    @FXML
+    private Label outputText;
     @FXML
     private ToggleButton toggleBtn;
     @FXML
@@ -22,5 +26,19 @@ public class MainController {
             audioManager.stop();
             System.out.println("Кнопка отжата");
         }
+    }
+
+    @FXML
+    public void initialize() {
+        Timeline timeline = new Timeline(
+                new KeyFrame(
+                        Duration.seconds(1),
+                        event -> {
+                            outputText.setText(audioManager.getText());
+                        }
+                )
+        );
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 }
